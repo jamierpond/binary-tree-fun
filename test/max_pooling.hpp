b05c94764b7b8b117924d175d6442872b6a468e6
Jamie Pond <73431532+jamierpond@users.noreply.github.com>
2025-01-22 00:29:24 -0800

    cool

--- test/max_pooling.hpp
+++ test/max_pooling.hpp
@@ -4,33 +4,44 @@
 
 namespace pond {
 
-template <typename T, size_t MaxSize>
+// you may not want to pay the performance cost of bounds checking
+// due to the extra branching.
+enum class BoundsChecking {
+  Disabled, // "Because this is C++" -- John Kalb
+  SilentReturn,
+  // ThrowException, // todo
+};
+
+template <typename T, size_t MaxSize, BoundsChecking BoundsCheckOption = BoundsChecking::Disabled>
 struct deque {
-  constexpr auto operator[](size_t i) const { return arr[(front_ptr + i) % MaxSize]; }
-  constexpr auto empty() const { return front_ptr == back_ptr; }
   constexpr auto pop_back() { back_ptr = decrement(back_ptr); }
   constexpr auto pop_front() { front_ptr = increment(front_ptr); }
+  constexpr auto push_front(T val) { emplace<decrement>(front_ptr, val); }
+  constexpr auto push_back(T val) { emplace<increment>(back_ptr, val); }
+
   constexpr auto size() const { return modulo(back_ptr - front_ptr); }
+  constexpr auto operator[](size_t i) const { return arr[(front_ptr + i) % MaxSize]; }
+  constexpr auto empty() const { return front_ptr == back_ptr; }
+  constexpr auto nearly_full() const { return increment(back_ptr) == front_ptr; }
+
   constexpr auto front() const { return arr[front_ptr]; }
   constexpr auto back() const {
     if (size() <= 1) { return arr[front_ptr]; }
     return arr[decrement(back_ptr)];
   }
 
-  constexpr auto push_back(T val) {
-    arr[back_ptr] = val;
-    back_ptr = increment(back_ptr);
-  }
-
-  constexpr auto push_front(T val) {
-    arr[front_ptr] = val;
-    front_ptr = decrement(front_ptr);
-  }
-
 private:
   std::array<T, MaxSize> arr{};
   size_t front_ptr = 0;
   size_t back_ptr = 0;
+  template <auto PointerFn>
+  constexpr auto emplace(size_t& ptr, T val) {
+    if constexpr (BoundsCheckOption == BoundsChecking::SilentReturn) {
+      if (nearly_full()) { return; }
+    }
+    arr[ptr] = val;
+    ptr = PointerFn(ptr);
+  }
   constexpr static auto modulo(size_t val) { return (val + MaxSize) % MaxSize; }
   constexpr static auto increment(size_t ptr) { return modulo(ptr + 1); }
   constexpr static auto decrement(size_t ptr) { return modulo(ptr - 1); }
@@ -59,6 +70,25 @@ static_assert([] {
   return true;
 }());
 
+
+// harder tests...
+static_assert([] {
+  auto dq = deque<int, 10, BoundsChecking::SilentReturn>{};
+  dq.push_back(1);
+  dq.push_back(2);
+  dq.push_back(3);
+  dq.push_back(4);
+  dq.push_back(5);
+  dq.push_back(6);
+  dq.push_back(7);
+  dq.push_back(8);
+  dq.push_back(9);
+  dq.push_back(10);
+  // silent return for bounds checking
+  if (dq.back() != 9) { return false; }
+  return true;
+}());
+
 } // namespace pond
 
 constexpr auto get_max_pool_out_size = [](auto N, auto K, auto Stride) {
@@ -67,12 +97,13 @@ constexpr auto get_max_pool_out_size = [](auto N, auto K, auto Stride) {
 
 template <typename T, size_t K>
 constexpr auto max_pool_1d(const T* arr, size_t N)  {
-  auto dq = pond::deque<size_t, K + 1>{};
   auto sz = get_max_pool_out_size(N, K, 1);
-  using RT = std::vector<T>;
-  auto result = RT(sz);
+  auto result = std::vector<T>(sz);
+
+  auto dq = pond::deque<size_t, K + 1>{};
 
   for (auto i = 0; i < N; i += 1) {
+
     auto initial_window = i < K - 1;
     if (!initial_window && !dq.empty() && dq.front() < i - K + 1) {
       dq.pop_front();
@@ -95,15 +126,6 @@ template <typename T, size_t N, size_t K>
 constexpr auto max_pool_1d(const std::array<T, N>& arr) {
   return max_pool_1d<T, K>(arr.data(), N);
 }
-
-constexpr auto array_equal = [](auto a, auto b) {
-  for (size_t i = 0; i < a.size(); i++) {
-    if (a[i] != b[i]) {
-      return false;
-    }
-  }
-  return true;
-};
 
 static_assert(max_pool_1d<int, 8, 3>({8, 3, -1, -3, 5, 3, 6, 7}) == std::vector<int>{8, 3, 5, 5, 6, 7});
 
