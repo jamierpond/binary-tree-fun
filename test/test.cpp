bf78c83db8f7e72034b26ae591fe64f7e9c0afa8
Jamie Pond <73431532+jamierpond@users.noreply.github.com>
2025-01-21 23:48:10 -0800

    deque for maxpooling

--- test/test.cpp
+++ test/test.cpp
@@ -1,10 +1,169 @@
+#include "catch2/catch_test_macros.hpp"
 #include "tree_utils.hpp"
+#include <cstddef>
 #define CATCH_CONFIG_MAIN
-#include <catch2/catch_all.hpp>
-#include <iostream>
-#include <cstdint>
 #include "bst.hpp"
+#include <catch2/catch_all.hpp>
+#include <cstdint>
+#include <iostream>
 
+constexpr auto get_max_pool_out_size = [](auto N, auto K, auto Stride) {
+  return (N / Stride) - K + 1;
+};
+
+
+/**
+
+0 0 0 0 0
+^
+^
+
+push_back(1)
+1 0 0 0 0
+  ^
+^
+
+pop_front()
+1 0 0 0 0
+  ^
+  ^
+
+push_back(2)
+1 2 0 0 0
+    ^
+  ^
+
+push_front(3)
+3 2 0 0 0
+    ^
+^
+
+pop_back()
+3 2 0 0 0
+  ^
+^
+
+//////////////////////////
+///
+push_front()
+0 0 0 1 0
+        ^
+      ^
+
+*/
+
+
+namespace pond {
+template <typename T, size_t MaxSize>
+struct deque {
+  constexpr auto operator[](size_t i) const { return arr[(front_ptr + i) % MaxSize]; }
+  constexpr auto empty() const { return front_ptr == back_ptr; }
+  constexpr auto pop_back() { back_ptr = decrement(back_ptr); }
+  constexpr auto pop_front() { front_ptr = increment(front_ptr); }
+  constexpr auto size() const { return modulo(back_ptr - front_ptr); }
+  constexpr auto front() const { return arr[front_ptr]; }
+  constexpr auto back() const {
+    if (size() == 1) { return arr[front_ptr]; }
+    return arr[decrement(back_ptr)];
+  }
+
+  constexpr auto push_back(T val) {
+    arr[back_ptr] = val;
+    back_ptr = increment(back_ptr);
+  }
+
+  constexpr auto push_front(T val) {
+    arr[front_ptr] = val;
+    front_ptr = decrement(front_ptr);
+  }
+
+private:
+  std::array<T, MaxSize> arr{};
+  size_t front_ptr = 0;
+  size_t back_ptr = 0;
+  constexpr static auto modulo(size_t val) { return (val + MaxSize) % MaxSize; }
+  constexpr static auto increment(size_t ptr) { return modulo(ptr + 1); }
+  constexpr static auto decrement(size_t ptr) { return modulo(ptr - 1); }
+};
+} // namespace pond
+
+TEST_CASE("deque works") {
+  auto dq = pond::deque<int, 10>{};
+  REQUIRE(dq.empty());
+  REQUIRE(dq.size() == 0);
+
+  dq.push_back(1);
+  dq.push_back(2);
+  dq.push_back(3);
+  dq.push_back(4);
+  REQUIRE(dq.back() == 4);
+  REQUIRE(dq.size() == 4);
+  REQUIRE(dq.front() == 1);
+  REQUIRE(!dq.empty());
+
+  dq.pop_back();
+  REQUIRE(dq.size() == 3);
+  REQUIRE(dq.back() == 3);
+
+  dq.pop_front();
+  REQUIRE(dq.size() == 2);
+  REQUIRE(dq.front() == 2);
+
+  dq.pop_front();
+  REQUIRE(dq.size() == 1);
+  REQUIRE(dq.front() == 3);
+
+  dq.pop_front();
+  REQUIRE(dq.size() == 0);
+  REQUIRE(dq.empty());
+};
+
+template <typename T, size_t K, size_t Stride = 1>
+constexpr auto max_pool_1d(const T* arr, size_t N)  {
+  auto dq = pond::deque<size_t, K + 1>{};
+  auto sz = get_max_pool_out_size(N, K, Stride);
+  using RT = std::vector<T>;
+  auto result= RT(sz);
+
+  for (int i = 0; i < N; i++) {
+    auto initial_window = i < K - 1;
+    if (not initial_window && !dq.empty() && dq.front() < i - K + 1) {
+      dq.pop_front();
+    }
+
+    while (!dq.empty() && arr[dq.back()] < arr[i]) {
+      dq.pop_back();
+    }
+
+    dq.push_back(i);
+
+    if (i >= K - 1) {
+      result[i - K + 1] = arr[dq.front()];
+    }
+  }
+  return result;
+}
+
+template <typename T, size_t N, size_t K, size_t Stride>
+constexpr auto max_pool_1d(const std::array<T, N>& arr) {
+  return max_pool_1d<T, K, Stride>(arr.data(), N);
+}
+
+constexpr auto array_equal = [](auto a, auto b) {
+  for (size_t i = 0; i < a.size(); i++) {
+    if (a[i] != b[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+TEST_CASE("max pooling algo works") {
+  auto arr = std::array<int, 8>{8, 3, -1, -3, 5, 3, 6, 7};
+  auto expected = std::array<int, 6>{8, 3, 5, 5, 6, 7};
+  auto out = max_pool_1d<int, 8, 3, 1>(arr);
+  REQUIRE(array_equal(out, expected));
+};
 
 TEST_CASE("extreme right insertion works") {
   auto tree = BinarySearchTree<int>{};
@@ -116,14 +275,13 @@ TEST_CASE("zero children node deletion") {
   std::cout << "+++++ smallest to biggest +++++" << std::endl;
 };
 
-
 auto fuzz = [](auto lower, auto upper) {
   auto tree = BinarySearchTree<int>{};
-  std::uint64_t seed = 0;
+  std::int64_t seed = 0;
 
   for (int i = 0; i < 1'000; i++) {
     auto next = rand() % (upper - lower) + lower;
-    auto* found = tree.find(next);
+    auto *found = tree.find(next);
     tree.insert(next);
   }
 
